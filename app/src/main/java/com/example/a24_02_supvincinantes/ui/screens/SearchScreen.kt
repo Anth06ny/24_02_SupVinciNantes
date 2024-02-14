2a566e63111e2df16e0a303d1cbf3f4f9dfb3ecf
AnthonyPCPortable <anth06ny@gmail.com>
2024-02-14 14:45:24 +0100

    TP Compose P2  + theme

--- app/src/main/java/com/example/a24_02_supvincinantes/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/example/a24_02_supvincinantes/ui/screens/SearchScreen.kt
@@ -2,19 +2,30 @@ package com.example.a24_02_supvincinantes.ui.screens
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,14 +54,55 @@ fun SearchScreenPreview() {
 @Composable
 fun SearchScreen() {
     //Couleur à retirer lors de l'utilisation des thèmes de couleur
-    Column(modifier = Modifier.background(Color.LightGray)) {
+    Column(modifier = Modifier
+        .background(MaterialTheme.colorScheme.outline)
+        .padding(8.dp)
 
-        repeat(pictureList.size) {
-            PictureRowItem(data = pictureList[it])
+    ) {
+
+        SearchBar()
+
+        Spacer(Modifier.size(8.dp))
+
+        LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.weight(1f)) {
+            items(pictureList.size) {
+                PictureRowItem(data = pictureList[it])
+            }
         }
 
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement=Arrangement.Center) {
 
+            Button(
+                onClick = { /* Do something! */ },
+                contentPadding = ButtonDefaults.ButtonWithIconContentPadding
+            ) {
+                Icon(
+                    Icons.Filled.Clear,
+                    contentDescription = "Localized description",
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+                Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                Text("Clear filter")
+            }
 
+            Spacer(Modifier.size(8.dp))
+
+            Button(
+                onClick = { /* Do something! */ },
+                contentPadding = ButtonDefaults.ButtonWithIconContentPadding
+            ) {
+                Icon(
+                    Icons.Filled.Send,
+                    contentDescription = "Localized description",
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+                Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                Text("Load data")
+            }
+        }
     }
 }
 
@@ -62,7 +114,7 @@ fun PictureRowItem(modifier: Modifier = Modifier, data: PictureBean) {
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .background(Color.White)
+            .background(MaterialTheme.colorScheme.onPrimary)
     ) {
 
 //Permission Internet nécessaire
@@ -93,10 +145,31 @@ fun PictureRowItem(modifier: Modifier = Modifier, data: PictureBean) {
             Text(
                 text = data.longText.take(20) + "...",
                 fontSize = 14.sp,
-                color = Color.Blue
+                color = MaterialTheme.colorScheme.primary
             )
         }
     }
 
 
+}
+
+
+@Composable
+fun SearchBar(modifier: Modifier = Modifier) {
+    TextField(
+        value = "", //Valeur par défaut
+        onValueChange = {newValue->}, //Action
+        leadingIcon = { //Image d'icone
+            Icon(
+                imageVector = Icons.Default.Search,
+                tint = MaterialTheme.colorScheme.primary,
+                contentDescription = null
+            )
+        },
+        label = { Text("Enter text") }, //Texte d'aide qui se déplace
+        //Comment le composant doit se placer
+        modifier = modifier
+            .fillMaxWidth() // Prend toute la largeur
+            .heightIn(min = 56.dp) //Hauteur minimum
+    )
 }
